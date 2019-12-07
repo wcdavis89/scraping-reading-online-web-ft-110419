9f2164635901b260e27a0191cd9e85aee013fe5b
Whittney Davis <wcdavis89@yahoo.com>
2019-12-07 23:41:27 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,3 +5,10 @@
 # doc.css(".headline-26OIBN").text.strip
 
 
+require 'nokogiri'
+require 'open-uri'
+ 
+html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+ 
+doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
