16e56a291f1e7210426e1ceebd8a208ab846812f
Whittney Davis <wcdavis89@yahoo.com>
2019-12-07 23:39:21 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,3 @@ require 'open-uri'
  
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 doc.css(".headline-26OIBN").text.strip
-
-html=open("https://flatironschool.com/")
-doc2=Nokogiri::HTML(html)
-doc2.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
