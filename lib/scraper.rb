22ffa7118cb213832591f9ed359021c126b4d106
Whittney Davis <wcdavis89@yahoo.com>
2019-12-08 00:19:48 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -15,9 +15,9 @@ doc.css(".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")
 
 courses=doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b.inlineMobileLeft-2Yo002 imageTextBlockGrid2-3jXtmC")
 
-# courses.each do |course|
-#   puts course.text.strip
-# end
+courses.each do |course|
+  puts course.text.strip
+end
 
 # p doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b.inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G")[0]
 
